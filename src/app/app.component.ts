afb8ea66ce31b99b2c344c29f3673e6368a06647
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-05-29 13:12:40 +0200

    exercici 4 talwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { ShipsListComponent } from './components/ships-list/ships-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ShipsListComponent],
+  imports: [RouterOutlet, ShipsListComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
