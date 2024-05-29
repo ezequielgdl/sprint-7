2ba020e3ccf975d3f73646ee41c72bb2371d445c
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-05-29 13:25:03 +0200

    exercici 5 add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { ShipsListComponent } from './components/ships-list/ships-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ShipsListComponent, RouterLink],
+  imports: [RouterOutlet, ShipsListComponent, RouterLink, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ShipsListComponent } from './components/ships-list/ships-list.component';
 import { ShipDetailsComponent } from './components/ship-details/ship-details.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: ShipsListComponent },
+  { path: 'starships', component: ShipsListComponent },
+  { path: '', component: HomeComponent },
   { path: 'starship/:id', component: ShipDetailsComponent },
 ];
